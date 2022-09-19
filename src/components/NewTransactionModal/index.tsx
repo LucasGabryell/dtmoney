bbdfb7bba2c9b70f3ab436b67d92f0c11ca9d255
Lucas Romao <lucas.gabryell@souunit.com.br>
2022-09-19 08:07:26 -0300

    form structure

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import ReactModal from "react-modal";
+import { Container } from "./styles";
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -8,8 +9,21 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <ReactModal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input placeholder="Título"></input>
+        <input placeholder="Valor" type="number"></input>
+        <input placeholder="Categoria"></input>
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </ReactModal>
   );
 }
