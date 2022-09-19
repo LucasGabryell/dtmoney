aa828de6e709051778c2d5a0aa0349976616439e
Lucas Romao <lucas.gabryell@souunit.com.br>
2022-09-19 07:20:42 -0300

    Configuring creation modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+import ReactModal from "react-modal";
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
+
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
         <button type="button">Nova Transação</button>
+        <ReactModal isOpen={isNewTransactionModalOpen}>
+          <h2>Cadastrar transação</h2>
+        </ReactModal>
       </Content>
     </Container>
   );
