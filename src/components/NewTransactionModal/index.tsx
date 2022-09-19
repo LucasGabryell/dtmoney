e7f9736a4ccdb1ccabe8a6abb036201ae2685704
Lucas Romao <lucas.gabryell@souunit.com.br>
2022-09-19 08:41:26 -0300

    Functioning of the Modal Buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import ReactModal from "react-modal";
-import { Container } from "./styles";
+import { Container, TransactionsTypeContainer } from "./styles";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -28,6 +30,17 @@ export function NewTransactionModal({
 
         <input placeholder="Título"></input>
         <input placeholder="Valor" type="number"></input>
+        <TransactionsTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionsTypeContainer>
         <input placeholder="Categoria"></input>
 
         <button type="submit">Cadastrar</button>
